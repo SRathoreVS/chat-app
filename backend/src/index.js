939c42da972e08c6775da8e9b5a4a4d55dfe06bf
SRathoreVS <satyamchelsea123@gmail.com>
2024-12-23 15:59:21 +0530

    Save changes before renaming branch

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,14 +3,17 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
+import exp from "constants";
 
 dotenv.config();
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 //middleware
 app.use(express.json());
@@ -25,6 +28,16 @@ app.use(
 //auth
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(
+      express.static(path.join(__dirname, "../frontend", "build", "index.html"))
+    );
+  });
+}
 
 server.listen(PORT, () => {
   console.log("Server is running on port : 5001");
